baaa18882f00936b49a49bb1756e52ccbd0a1923
Naxxo <kovacskristof140816@gmail.com>
2026-02-05 08:43:15 +0100

    Delete KnobDupe.lua

--- KnobDupe.lua
+++
@@ -1,8 +0,0 @@
-local CurrentKnobs = require(game.ReplicatedStorage.ReplicaDataModule).data.Knobs
-
-local ItemTable = {}
-for _ = 1, CurrentKnobs / 100 do -- 100 is the item price
-    table.insert(ItemTable, "Vitamins")
-end
-
-game.ReplicatedStorage.RemotesFolder.PreRunShop:FireServer(ItemTable)
